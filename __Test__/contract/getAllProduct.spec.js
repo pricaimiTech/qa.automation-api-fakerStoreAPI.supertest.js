292e556b801851d6e2da18ca4144708f1d406218
Priscila Caimi <priscila.caimi@hotmail.com>
2025-09-07 21:24:57 -0300

    teste de contrato

--- __Test__/contract/getAllProduct.spec.js
+++ __Test__/contract/getAllProduct.spec.js
@@ -1,14 +1,19 @@
 const {
     getRequest
 } = require('../../src/helpers/request');
+const { validateSchema } = require('../../src/helpers/schemaValidator');
+const schema = require('../../src/schema/products.schema.json');
 
 describe('Contract: Get All Products', () => {
-    
+
     let response;
     const path = '/products';
 
-    it('Request', async() => {
-        response = await getRequest(path);
-        console.log('Response:', response);
+    beforeAll(async() => {
+        response = await getRequest(path)
     });
+
+    it('Validate Contract', () => {
+        validateSchema(response, schema);
+    })
 });
